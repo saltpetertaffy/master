68b60124096f317d18658ae8eb6e6944a6243f1f
Brandon <saltpeter@gmail.com>
2020-09-07 23:39:28 -0400

    Death behaviors expanded

---
+++ Assets/Scripts/ParticleDeathBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDeathBehavior : DeathBehavior
+{
+    [SerializeField] ParticleSystem deathParticles;
+
+    public override void OnDeath() {
+        Instantiate(deathParticles, transform.localPosition, Quaternion.identity);
+        StartCoroutine(DelayDestroy(deathParticles.main.duration));;
+    }
+
+    private IEnumerator DelayDestroy(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        Destroy(deathParticles);
+        isCompleted = true;
+    }
+}
