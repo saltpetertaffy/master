a21f7382b6d36c225c156d3a70f1b8610d77074a
Brandon <saltpeter@gmail.com>
2020-09-08 07:09:27 -0400

    Method extracted

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -15,10 +15,24 @@ public class Projectile : MonoBehaviour
     }
 
     private void Update() {
+        GetComponent<SpriteRenderer>().color = generateDamageColor();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (projectileCollider.IsTouchingLayers(LayerMask.GetMask(GameKeys.LAYER_HITBOX_KEY))) {
+            Hitbox hitbox = collision.gameObject.GetComponent<Hitbox>();
+            if (hitbox) {
+                hitbox.HandleHit(projectileEffects);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    private Color generateDamageColor() {
         int totalDamage = 0;
 
         foreach (GameStatEffect projectileEffect in projectileEffects) {
-            switch(projectileEffect.GetGameStatEffectId()) {
+            switch (projectileEffect.GetGameStatEffectId()) {
                 case GameConfigConstants.EFFECT_ID_DAMAGE:
                     totalDamage += Mathf.RoundToInt(projectileEffect.GetValue());
                     break;
@@ -31,16 +45,7 @@ public class Projectile : MonoBehaviour
             }
         }
         float damageRedShade = totalDamage / (float) mainCharacterHealth.GetHealth();
-        GetComponent<SpriteRenderer>().color = new Color(1, 1 - damageRedShade, 1 - damageRedShade);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (projectileCollider.IsTouchingLayers(LayerMask.GetMask(GameKeys.LAYER_HITBOX_KEY))) {
-            Hitbox hitbox = collision.gameObject.GetComponent<Hitbox>();
-            if (hitbox) {
-                hitbox.HandleHit(projectileEffects);
-            }
-            Destroy(gameObject);
-        }
+        Color damageColor = new Color(1, 1 - damageRedShade, 1 - damageRedShade);
+        return damageColor;
     }
 }
