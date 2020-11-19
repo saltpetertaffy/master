c8e035acd2aaf35fa7b59ac7453020f112df6ce4
Brandon <saltpeter@gmail.com>
2020-11-19 14:51:38 -0500

    Upgrades class removed.

--- Assets/Scripts/Game Stats/Upgrades.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Upgrades : MonoBehaviour
-{
-    public List<Upgrade> UpgradeList { get; } = new List<Upgrade>();
-}
